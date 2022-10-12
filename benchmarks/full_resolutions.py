e18d8c471de407bf5e825b594154f3d4856638ad
Matthieu Ancellin <matthieu.ancellin@eurobios.com>
2022-10-12 15:21:00 +0200

    Add benchmarks of symmetries.

---
+++ benchmarks/full_resolutions.py
@@ -0,0 +1,45 @@
+import numpy as np
+import xarray as xr
+import capytaine as cpt
+
+
+def time_heaving_hemisphere():
+    sphere = cpt.Sphere(
+            radius=1.0, center=(0, 0, 0),
+            ntheta=50, nphi=50,
+            axial_symmetry=False,
+            )
+    sphere.keep_immersed_part()
+    sphere.add_translation_dof(name="Heave")
+
+    test_matrix = xr.Dataset(coords={
+        'omega': np.linspace(0.1, 4.0, 10),
+        'radiating_dof': ["Heave"]
+    })
+    engine = cpt.BasicMatrixEngine(linear_solver="direct")
+    green_function = cpt.Delhommeau()
+    solver = cpt.BEMSolver(engine=engine, green_function=green_function)
+
+    ds = solver.fill_dataset(test_matrix, sphere)
+
+
+def time_rigid_barge():
+    barge = cpt.RectangularParallelepiped(
+            size=(1.0, 1.0, 1.0),
+            resolution=(20, 20, 20),
+            reflection_symmetry=False,
+            translational_symmetry=False,
+            )
+    barge.keep_immersed_part()
+    barge.add_all_rigid_body_dofs()
+
+    test_matrix = xr.Dataset(coords={
+        'omega': np.linspace(0.1, 4.0, 10),
+        'radiating_dof': list(barge.dofs.keys()),
+    })
+    engine = cpt.BasicMatrixEngine(linear_solver="direct")
+    green_function = cpt.Delhommeau()
+    solver = cpt.BEMSolver(engine=engine, green_function=green_function)
+
+    ds = solver.fill_dataset(test_matrix, barge)
+
--- benchmarks/symmetries.py
+++ benchmarks/symmetries.py
@@ -3,12 +3,11 @@ import xarray as xr
 import capytaine as cpt
 
 
-def time_heaving_hemisphere():
+def time_heaving_hemisphere_with_circulant_matrix():
     sphere = cpt.Sphere(
             radius=1.0, center=(0, 0, 0),
             ntheta=50, nphi=50,
-            axial_symmetry=False,
-            name="sphere"
+            axial_symmetry=True,
             )
     sphere.keep_immersed_part()
     sphere.add_translation_dof(name="Heave")
@@ -17,20 +16,18 @@ def time_heaving_hemisphere():
         'omega': np.linspace(0.1, 4.0, 10),
         'radiating_dof': ["Heave"]
     })
-    engine = cpt.BasicMatrixEngine(linear_solver="direct")
+    engine = cpt.HierarchicalToeplitzMatrixEngine()
     green_function = cpt.Delhommeau()
     solver = cpt.BEMSolver(engine=engine, green_function=green_function)
 
     ds = solver.fill_dataset(test_matrix, sphere)
 
-
-def time_rigid_barge():
-    # No symmetry by default
+def time_rigid_barge_with_toeplitz_matrix():
     barge = cpt.RectangularParallelepiped(
             size=(1.0, 1.0, 1.0),
             resolution=(20, 20, 20),
             reflection_symmetry=False,
-            translational_symmetry=False,
+            translational_symmetry=True,
             )
     barge.keep_immersed_part()
     barge.add_all_rigid_body_dofs()
@@ -39,9 +36,8 @@ def time_rigid_barge():
         'omega': np.linspace(0.1, 4.0, 10),
         'radiating_dof': list(barge.dofs.keys()),
     })
-    engine = cpt.BasicMatrixEngine(linear_solver="direct")
+    engine = cpt.HierarchicalToeplitzMatrixEngine()
     green_function = cpt.Delhommeau()
     solver = cpt.BEMSolver(engine=engine, green_function=green_function)
 
     ds = solver.fill_dataset(test_matrix, barge)
-
