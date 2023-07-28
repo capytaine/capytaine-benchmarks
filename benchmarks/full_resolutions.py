795300cbba4070013b00284eb341635f56e20b45
Matthieu Ancellin <matthieu.ancellin@eurobios.com>
2023-07-28 16:37:47 +0200

    WIP Update benchmarks.

--- benchmarks/full_resolutions.py
+++ benchmarks/full_resolutions.py
@@ -3,26 +3,29 @@ import xarray as xr
 import capytaine as cpt
 
 
-def time_heaving_hemisphere():
-    sphere = cpt.Sphere(
-            radius=1.0, center=(0, 0, 0),
-            ntheta=50, nphi=50,
-            axial_symmetry=False,
-            )
-    sphere.keep_immersed_part()
-    sphere.add_translation_dof(name="Heave")
+# def time_heaving_hemisphere(fp):
+#     sphere = cpt.Sphere(
+#             radius=1.0, center=(0, 0, 0),
+#             ntheta=50, nphi=50,
+#             axial_symmetry=False,
+#             )
+#     sphere.keep_immersed_part()
+#     sphere.add_translation_dof(name="Heave")
+#
+#     test_matrix = xr.Dataset(coords={
+#         'omega': np.linspace(0.1, 4.0, 10),
+#         'radiating_dof': ["Heave"]
+#     })
+#     green_function = cpt.Delhommeau(floating_point_precision=fp)
+#     solver = cpt.BEMSolver(green_function=green_function)
+#
+#     ds = solver.fill_dataset(test_matrix, sphere)
+#
+# time_heaving_hemisphere.params = (["float32", "float64"],)
+# time_heaving_hemisphere.param_names = ["floating_point_precision"]
 
-    test_matrix = xr.Dataset(coords={
-        'omega': np.linspace(0.1, 4.0, 10),
-        'radiating_dof': ["Heave"]
-    })
-    engine = cpt.BasicMatrixEngine(linear_solver="direct")
-    green_function = cpt.Delhommeau()
-    solver = cpt.BEMSolver(engine=engine, green_function=green_function)
 
-    ds = solver.fill_dataset(test_matrix, sphere)
-
-
+# def time_rigid_barge(fp):
 def time_rigid_barge():
     barge = cpt.RectangularParallelepiped(
             size=(1.0, 1.0, 1.0),
@@ -37,9 +40,11 @@ def time_rigid_barge():
         'omega': np.linspace(0.1, 4.0, 10),
         'radiating_dof': list(barge.dofs.keys()),
     })
-    engine = cpt.BasicMatrixEngine(linear_solver="direct")
-    green_function = cpt.Delhommeau()
-    solver = cpt.BEMSolver(engine=engine, green_function=green_function)
+    # green_function = cpt.Delhommeau(floating_point_precision=fp)
+    # solver = cpt.BEMSolver(green_function=green_function)
 
-    ds = solver.fill_dataset(test_matrix, barge)
+    solver = cpt.BEMSolver()
+    ds = solver.fill_dataset(test_matrix, [barge])
 
+# time_rigid_barge.params = (["float64"],)
+# time_rigid_barge.param_names = ["floating_point_precision"]
